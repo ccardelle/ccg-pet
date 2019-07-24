70852cc3a8e2dcd5c7859fa40f768f243c73a9be
Chris Cardelle <chris.cardelle@gmail.com>
2019-07-23 22:34:16 -0400

    Added onChange handlers to filters

--- client/src/components/pages/AvailablePets.jsx
+++ client/src/components/pages/AvailablePets.jsx
@@ -7,20 +7,54 @@ export default class AvailablePets extends Component {
     this.state = {
       availablePets: [],
       filteredPets: [],
+      petType: 'All',
+      petBreed: 'All',
+      petGender: 'All',
     }
+
+    this.handleTypeChange = this.handleTypeChange.bind(this)
+    this.handleBreedChange = this.handleBreedChange.bind(this)
+    this.handleGenderChange = this.handleGenderChange.bind(this)
   }
+
+  handleTypeChange(event) {
+    this.setState({ petType: event.target.value })
+  }
+
+  handleBreedChange(event) {
+    this.setState({ petBreed: event.target.value })
+  }
+
+  handleGenderChange(event) {
+    this.setState({ petGender: event.target.value })
+  }
+
   render() {
     return (
       <div className="pet-page">
         <h2>List of Available Pets</h2>
         <div className="pet-filters">
           <span className="filter-label">Pet Type: </span>
-          <select className="filter" name="select-type">
+          <select
+            className="filter"
+            name="select-type"
+            onChange={this.handleTypeChange}
+            value={this.state.petType}
+          >
+            <option value="All">All</option>
             <option value="Dog">Dog</option>
             <option value="Cat">Cat</option>
           </select>
+
           <span className="filter-label">Pet Breed: </span>
-          <select className="filter" name="select-breed">
+
+          <select
+            className="filter"
+            name="select-breed"
+            onChange={this.handleBreedChange}
+            value={this.state.petBreed}
+          >
+            <option value="All">All</option>
             <option value="Huskie">Huskie</option>
             <option value="Poodle">Poodle</option>
             <option value="German Shephard">German Shephard</option>
@@ -34,7 +68,13 @@ export default class AvailablePets extends Component {
             <option value="Mix">Mix</option>
           </select>
           <span className="filter-label">Pet Gender: </span>
-          <select className="filter" name="select-gender">
+          <select
+            className="filter"
+            name="select-gender"
+            onChange={this.handleGenderChange}
+            value={this.state.petGender}
+          >
+            <option value="All">All</option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
           </select>
@@ -44,6 +84,7 @@ export default class AvailablePets extends Component {
             <div key={p._id} className="pet-card">
               <img className="pet-img" src={p.imgUrl} alt="" />
               <h1>{p.name}</h1>
+              <hr />
               Type: {p.type} Gender: {p.gender} Breed: {p.breed}
             </div>
           ))}
